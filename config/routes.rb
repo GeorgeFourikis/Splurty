aa8bc2cebc7a404ecb9f6f02a68e57a8290398f0
vagrant <you@example.cogit config --global user.email you@example.co>
2016-04-05 16:27:20 +0000

    FORM

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Splurty::Application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'quotes#index'
 
+   resources :quotes
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
